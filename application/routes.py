b4cda1fda3dda3d9071b942d1db0cf90bd928786
ChanChoonQi <2b12.2110.chanchoonqi@gmail.com>
2021-05-13 20:47:39 +0800

    "Ensuring that it can work with flask"

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,10 @@
 from application import app
 from flask import render_template , request , flash , redirect , url_for , session
-
+#from application.form_login import LoginForm
 
 # This section set up the route for login page
+@app.route('/')
 @app.route('/login')
 def loginPage():
-    return "Hello"
+    #form = LoginForm()
+    return render_template('login.html', title = "Login")
