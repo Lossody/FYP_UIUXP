2f7aa5de56510e75be8877df029e614dca38125c
Chan Choon Qi <2b12.2110.chanchoonqi@gmail.com>
2021-05-24 16:27:27 +0800

    "Setting up database"

--- application/feedback_model.py
+++ application/feedback_model.py
@@ -4,8 +4,9 @@ from application import db
 class Feedback_Entry(db.Model):
     __tablename__ = "Feedback_Table"
 
-    id = db.Column(db.Integer,primary_key = True, autoincrement = True)
+    feedback_id = db.Column(db.Integer,primary_key = True, autoincrement = True)
     user_id = db.Column(db.Integer)
     username = db.Column(db.String)
+    category = db.Column(db.String)
     feedback = db.Column(db.String)
     position = db.Column(db.String)
