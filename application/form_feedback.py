7581d654e9a93296aed4ef718564658f3c93e5ee
Damian Tan <damiantanhy19.19@ichat.sp.edu.sg>
2021-05-26 17:34:51 +0800

    Feedback Prototype

--- application/form_feedback.py
+++ application/form_feedback.py
@@ -3,7 +3,7 @@ from wtforms import SubmitField , StringField, RadioField , SelectField
 from wtforms.validators import Length, InputRequired , ValidationError , NumberRange , Regexp
 
 class FeedbackForm(FlaskForm):
-    stars = RadioField('Stars',choices=[('5','5'),('4','4'),('3','3'),('2','2'),('1','1')],validators = [InputRequired()])
+    stars = RadioField('Stars',choices=[('1','Very Unsatisifed'),('2','UnSatisfied'),('3','Somewhat Satisifed'),('4','Satisfied'),('5','Very Satisfied')],validators = [InputRequired()])
     category = SelectField('Password',choices=[('A','A'),('B','B'),('C','C')],validators = [InputRequired()])
     feedback = StringField('feedback',validators = [Length(1,300)])
     submit = SubmitField("Submit")
